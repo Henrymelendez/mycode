b4dbed80de3d0021e8ba4949d88e80aeda02ed49
Henrymelendez <henryomarm@gmail.com>
2023-01-09 21:24:00 +0000

    challenge 18

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+
+    input_user = input("what is your user name?");
+
+    input_date = input("What is the day of the week?");
+
+
+
+    print("Hello, " + input_user + ": Happy " + input_date + "!");
+
+
+main()

25c2ef7fe3349eab326231cde18e6a28085e103b
Henrymelendez <henryomarm@gmail.com>
2023-01-11 17:24:26 +0000

    working on if, elif, else logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Conditionals - strings test true"""
+
+ipchk = input("Apply an IP address: ");
+
+# a string tests as True
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk)
+else:
+    print("you did not provide input");
+
+

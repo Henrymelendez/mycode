5b9098fc0491dcbee65059d12585c90afe303d80
Henrymelendez <henryomarm@gmail.com>
2023-01-17 20:25:05 +0000

    reveiwing how to manipulate JSON data with python

--- mtg/apiMTG07.py
+++ mtg/apiMTG07.py
@@ -17,18 +17,16 @@ API = "https://api.magicthegathering.io/v1/" # this will never change regardless
 def main():
     """Run time code"""
 
+    setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
+
     # create resp, which is our request object
-    resp = requests.get(f"{API}sets")   # this "f" string reads: API + "sets"
-                                        # OR, https://api.magicthegathering.io/v1/sets
+    resp = requests.get(f"{API}cards?set={setcode}")   # this "f" string reads: API + "cards/" + setcode
 
     # the .json() method will dump a JSON string into Pythonic data structures. COOL!
     # This is much easier than using the urllib.request library
-    cardsets = resp.json().get("sets")
-    # open a file we write our data into
-    with open("mtgsets.index", "w") as mtgfile:
-        for cardset in cardsets:  # loop across ALL of the sets of MTG cards (they are stored as dict objects)
-            print(f"{cardset.get('name')} -- {cardset.get('code')}", file=mtgfile)  # write the data "we want"
-                                                                                    # into mtgfile
+    cards = resp.json()
+
+    print(cards)
 
 if __name__ == "__main__":
     main()

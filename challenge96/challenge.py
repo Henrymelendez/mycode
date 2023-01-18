beefe5ae66baeb93cb71f814362524a4b99b9819
Henrymelendez <henryomarm@gmail.com>
2023-01-18 21:24:47 +0000

    Challenge 96

---
+++ challenge96/challenge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Friday Warmup | Returning Data From Complex JSON"""
+
+import requests
+
+URL= "https://opentdb.com/api.php?amount=3&category=10&difficulty=easy&type=multiple"
+
+def main():
+
+    # data will be a python dictionary rendered from your API link's JSON!
+    data= requests.get(URL).json()
+    print(data["results"])
+    print('\n')
+    answers = []
+    
+    for qna in data["results"]:
+
+        print(qna["question"])
+        answers.append(qna["correct_answer"])
+
+        for i in qna["incorrect_answers"]:
+            answers.append(i)
+        
+        for ans in answers:
+            print(ans)
+
+
+        print("\n")
+
+
+        answers = []
+if __name__ == "__main__":
+    main()
+
